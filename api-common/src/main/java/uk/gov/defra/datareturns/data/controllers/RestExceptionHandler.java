d0c06d0457b6ace179143cb26da705b44af0261f
Sam Gardner-Dell <sgardnerdell.ea@gmail.com>
2018-10-26 11:34:34 +0100

    Add special ExceptionHandler for nested AccessDeniedExceptions

--- api-common/src/main/java/uk/gov/defra/datareturns/data/controllers/RestExceptionHandler.java
+++ api-common/src/main/java/uk/gov/defra/datareturns/data/controllers/RestExceptionHandler.java
@@ -3,20 +3,26 @@ package uk.gov.defra.datareturns.data.controllers;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Value;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.MessageSourceAccessor;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.data.rest.core.RepositoryConstraintViolationException;
 import org.springframework.data.rest.core.ValidationErrors;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.stereotype.Component;
 import org.springframework.util.Assert;
 import org.springframework.validation.FieldError;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import javax.inject.Inject;
 import java.util.ArrayList;
@@ -30,8 +36,9 @@ import java.util.List;
  */
 @ControllerAdvice(basePackageClasses = org.springframework.data.rest.webmvc.RepositoryRestExceptionHandler.class)
 @Order(Ordered.HIGHEST_PRECEDENCE)
+@Slf4j
 @Component
-public class RestExceptionHandler {
+public class RestExceptionHandler extends ResponseEntityExceptionHandler {
     private final MessageSourceAccessor messageSourceAccessor;
 
     /**
@@ -43,6 +50,29 @@ public class RestExceptionHandler {
     public RestExceptionHandler(final ApplicationContext applicationContext) {
         this.messageSourceAccessor = new MessageSourceAccessor(applicationContext);
     }
+
+
+    /**
+     * Handle {@link HttpMessageNotReadableException}s - extends the default functionality to intercept exceptions generated where the root
+     * cause is an {@link AccessDeniedException} and issue a suitable response code for these cases.
+     *
+     * @param ex      the exception
+     * @param headers the headers to be written to the response
+     * @param status  the selected response status
+     * @param request the current request
+     * @return a {@code ResponseEntity} instance
+     */
+    @Override
+    protected ResponseEntity<Object> handleHttpMessageNotReadable(final HttpMessageNotReadableException ex, final HttpHeaders headers,
+                                                                  final HttpStatus status, final WebRequest request) {
+        Throwable root = ex.getMostSpecificCause();
+        if (root instanceof AccessDeniedException) {
+            AccessDeniedError error = AccessDeniedError.of("Access to associated resource denied", ex.getMessage());
+            return super.handleExceptionInternal(ex, error, new HttpHeaders(), HttpStatus.FORBIDDEN, request);
+        }
+        return super.handleHttpMessageNotReadable(ex, headers, status, request);
+    }
+
 
     /**
      * Handles {@link RepositoryConstraintViolationException}s by returning {@code 400 Bad Request}.
@@ -103,9 +133,7 @@ public class RestExceptionHandler {
     }
 
     /**
-     * Simple that will be serialized to the response for each validation error encountered
-     *
-     * @author Sam Gardner-Dell
+     * Response data for constraint violation exceptions
      */
     @Value(staticConstructor = "of")
     private static class ValidationError {
@@ -115,4 +143,12 @@ public class RestExceptionHandler {
         private String message;
     }
 
+    /**
+     * Response data for access denied error
+     */
+    @Value(staticConstructor = "of")
+    private static class AccessDeniedError {
+        private String error;
+        private String cause;
+    }
 }
